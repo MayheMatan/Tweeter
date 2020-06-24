d8672291abcc20488c5c0763481ab6e1b16d744d
myusername <Matanfriedleet@hotmail.com>
2020-06-24 12:07:23 +0300

    fixes added to logic.js

--- logic.js
+++ logic.js
@@ -1,5 +1,5 @@
 const Tweeter = () => {
-    let _posts = [{
+    const _posts = [{
             text: "First post!",
             id: "p1",
             comments: [
@@ -18,21 +18,26 @@ const Tweeter = () => {
             ]
         }
     ];
-
-    let postIdCounter = () => _posts.length;
-    let commentIdCounter = () => _posts.map(post => post.comments.length).reduce((a, b) => a + b);
+    let postIdCounter = _posts.length;
+    let commentIdCounter = _posts.map(post => post.comments.length).reduce((a, b) => a + b);
 
     const getPosts = () => _posts;
 
-    const addPost = text => _posts.push({ text: text, id: "p" + parseInt(postIdCounter() + 1), comments: [] });
+    const addPost = text => _posts.push({ text: text, id: "p" + parseInt(postIdCounter + 1), comments: [] });
 
-    const removePost = postId => _posts = _posts.filter(post => post.id !== postId);
+    const removePost = postId => {
+        for (let post in _posts) {
+            if (_posts[post].id === postId) {
+                _posts.splice(post, 1);
+            }
+        }
+    }
 
     const addComment = (text, postId) => {
         for (let post of _posts) {
             if (post.id === postId) {
-                post.comments.push({ id: "c" + parseInt(commentIdCounter() + 1), text: text });
-                break;
+                post.comments.push({ id: "c" + parseInt(commentIdCounter + 1), text: text });
+                return;
             }
         }
     }
@@ -40,8 +45,12 @@ const Tweeter = () => {
     const removeComment = (postId, commentId) => {
         for (let post of _posts) {
             if (post.id === postId) {
-                post.comments = post.comments.filter(comment => comment.id !== commentId);
-                break;
+                for (let comment in post.comments) {
+                    if (post.comments[comment].id === commentId) {
+                        post.comments.splice(comment, 1);
+                        return;
+                    }
+                }
             }
         }
     }
@@ -51,34 +60,34 @@ const Tweeter = () => {
 
 const tweeter = Tweeter()
 
-// tweeter.addPost("This is my own post!")
-// console.log(tweeter.getPosts())
-//     //This should be added to the posts array:
-//     //{text: "This is my own post!", id: "p3", comments: []}
+tweeter.addPost("This is my own post!")
+console.log(tweeter.getPosts())
+    //This should be added to the posts array:
+    //{text: "This is my own post!", id: "p3", comments: []}
 
 // tweeter.removePost("p1")
 // console.log(tweeter.getPosts())
-// //There should only be two posts in the post's array:
-// //{text: "Aw man, I wanted to be first", id: "p2", comments: Array(3)}
-// //{text: "This is my own post!", id: "p3", comments: []}
+//     //There should only be two posts in the post's array:
+//     //{text: "Aw man, I wanted to be first", id: "p2", comments: Array(3)}
+//     //{text: "This is my own post!", id: "p3", comments: []}
 
-// //============================
-// //============================
-// //Stop here
-// //Make sure everything works. Then keep going
-// //============================
-// //============================
+//============================
+//============================
+//Stop here
+//Make sure everything works. Then keep going
+//============================
+//============================
 
-// tweeter.addComment("Damn straight it is!", "p3")
-// tweeter.addComment("Second the best!", "p2")
-// console.log(tweeter.getPosts())
-//This should be added to the third post's comments array:
-//{id: "c7", text: "Damn straight it is!"}
+tweeter.addComment("Damn straight it is!", "p3")
+tweeter.addComment("Second the best!", "p2")
+console.log(tweeter.getPosts())
+    // This should be added to the third post's comments array:
+    // {id: "c7", text: "Damn straight it is!"}
 
-//This should be added to the second post's comments array:
-//{id: "c8", text: "Second the best!"}
+// This should be added to the second post's comments array:
+// {id: "c8", text: "Second the best!"}
 
 tweeter.removeComment("p2", "c6")
 console.log(tweeter.getPosts())
-    //This comment should be removed:
-    //{id: "c6", text: "Haha second place what a joke."}
+    // This comment should be removed:
+    // {id: "c6", text: "Haha second place what a joke."}
